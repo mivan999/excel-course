b7d5abd7a83c75c6223fbb8067cb5257620894a5
Ivan Mulyavin <mivan999@mail.ru>
2021-04-19 15:23:37 +0700

    25les -7:55

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const path=require('path')
-const HTMLWwebpackPlugin =require('html-webpack-plugin')
+const HtmlWebpackPlugin =require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyPlugin = require("copy-webpack-plugin")
+const CopyPlugin = require('copy-webpack-plugin')
 
 const isProd = process.env.NODE_ENV==='production'
 const isDev = !isProd
@@ -12,7 +12,7 @@ const filename=ext=>isDev? `bundle.${ext}`:`bundle.[hash].${ext}`
 const jsLoaders=()=>{
   const loaders = [
     {
-      loader: "babel-loader",
+      loader: 'babel-loader',
       options: {
         presets: ['@babel/preset-env']
       }
@@ -22,83 +22,83 @@ const jsLoaders=()=>{
     loaders.push('eslint-loader')
   }
 
-    return loaders
+  return loaders
 }
 module.exports={
-    context: path.resolve(__dirname, 'src'), //указывает где лажат исходники
+  context: path.resolve(__dirname, 'src'), // указывает где лажат исходники
 
-    mode: 'development',
-    entry: ['@babel/polyfill','./index.js'], //start point/ babel for async
-    output: {
-        filename: filename('js'),
-        path: path.resolve(__dirname, 'dist')
-    },
-    resolve: {
-        extensions: ['.js'],
-        alias: {
-            '@': path.resolve(__dirname,'src'),
-            '@core': path.resolve(__dirname, 'src/core')
-        }
-    },
-    devtool: isDev ? 'source-map' : false,
-    //watch: true,//default in dev-server
-    target: 'web',
-    // watchOptions: {
-    //     ignored: /node_modules/,
-    //     aggregateTimeout: 200,
-    //     poll: 1000,
-    // },
-    devServer: {
-        //contentBase: path.resolve(__dirname,'src'),
-        //watchContentBase: true,
-        port: 3000,
-        hot: isDev
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HTMLWwebpackPlugin({
-            template: 'index.html',
-            minify: {
-                removeComments: isProd,
-                collapseWhitespace: isProd
-            },
+  mode: 'development',
+  entry: ['@babel/polyfill', './index.js'], // start point/ babel for async
+  output: {
+    filename: filename('js'),
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@core': path.resolve(__dirname, 'src/core')
+    }
+  },
+  devtool: isDev ? 'source-map' : false,
+  // watch: true,//default in dev-server
+  target: 'web',
+  // watchOptions: {
+  //     ignored: /node_modules/,
+  //     aggregateTimeout: 200,
+  //     poll: 1000,
+  // },
+  devServer: {
+    // contentBase: path.resolve(__dirname,'src'),
+    // watchContentBase: true,
+    port: 3091,
+    hot: isDev
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      minify: {
+        removeComments: isProd,
+        collapseWhitespace: isProd
+      },
 
-        }),
-        new CopyPlugin({
-            patterns: [
-                { from: path.resolve(__dirname, 'src/favicon.ico'),
-                    to: path.resolve(__dirname, 'dist')}
+    }),
+    new CopyPlugin({
+      patterns: [
+        {from: path.resolve(__dirname, 'src/favicon.ico'),
+          to: path.resolve(__dirname, 'dist')}
 
-            ],
-        }),
-        new MiniCssExtractPlugin({
-            filename: filename('css')
-        })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    {
-                       loader: MiniCssExtractPlugin.loader,
-                        options: {
-                           //hmr: isDev,
-                          //  reloadAll: true //reload scss
-                        }
-                    },
-
-                    // Translates CSS into CommonJS
-                    "css-loader",
-                    // Compiles Sass to CSS
-                    "sass-loader"
-                ],
-            },
-            {
-                test: /\.m?js$/,
-                exclude: /node_modules/,
-                use: jsLoaders()
+      ],
+    }),
+    new MiniCssExtractPlugin({
+      filename: filename('css')
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              // hmr: isDev,
+              //  reloadAll: true //reload scss
             }
+          },
+
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader'
         ],
-    },
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: jsLoaders()
+      }
+    ],
+  },
 }
